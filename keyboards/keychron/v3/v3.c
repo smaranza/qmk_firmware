9a41e9666b762db5db673c8502c30884c81ba5b3
Silvia Maranzan <silvia@Air-di-Smaranza.local>
2023-05-02 18:35:30 +0200

    v05 (fixed alpha flag)

--- keyboards/keychron/v3/v3.c
+++ keyboards/keychron/v3/v3.c
@@ -78,8 +78,8 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
         for (uint8_t i = led_min; i < led_max; i++) {
             if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-                rgb_matrix_set_color(i, RGB_GOLD);
-                // RGB_MATRIX_INDICATOR_SET_COLOR(i, 255, 255, 255);
+                // rgb_matrix_set_color(i, RGB_GOLD);
+                RGB_MATRIX_INDICATOR_SET_COLOR(i, 80, 80, 80);
             }
         }
     } else {
